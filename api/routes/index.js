352133d84a200626aed6731c77fe2a5d239b4a49
Edmir Lôbo <ejuniorls@outlook.com>
2023-11-02 13:22:37 -0300

    aula 03 - 3.3 - Organizando as rotas

---
+++ api/routes/index.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser');
+
+module.exports = app => {
+    app.use(bodyParser.json());
+
+    app.get('/', (req, res) => res
+        .status(200)
+        .send({
+            mensagem: 'Olá!'
+        }));
+};
