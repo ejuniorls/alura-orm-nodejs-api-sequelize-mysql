1f134541ef28695ac6db4d787215f0d2b9ee9778
Edmir Lôbo <ejuniorls@outlook.com>
2023-11-02 20:03:47 -0300

    aula 04 - 2.2 - Métodos de CRUD

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,19 +10,19 @@ class PessoaController {
         }
     }
 
-    // static async pegaUmaPessoa(req, res) {
-    //     const { id } = req.params
-    //     try {
-    //         const umaPessoa = await database.Pessoas.findOne({
-    //             where: {
-    //                 id: Number(id)
-    //             }
-    //         })
-    //         return res.status(200).json(umaPessoa)
-    //     } catch (error) {
-    //         return res.status(500).json(error.message)
-    //     }
-    // }
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(umaPessoa)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 
     // static async criaPessoa(req, res) {
     //     const novaPessoa = req.body
