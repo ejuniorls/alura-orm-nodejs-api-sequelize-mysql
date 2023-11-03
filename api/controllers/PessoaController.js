b131e40f6dbb9c1d7fa62d96baf700dc038be312
Edmir Lôbo <ejuniorls@outlook.com>
2023-11-03 09:07:25 -0300

    aula 04 - 4.5 - CRUD: Atualizar e deletar

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,6 +1,7 @@
 const database = require('../models')
 
 class PessoaController {
+    // listar todos registros
     static async pegaTodasAsPessoas(req, res) {
         try {
             const todasAsPessoas = await database.Pessoas.findAll()
@@ -10,6 +11,7 @@ class PessoaController {
         }
     }
 
+    // listar um registro
     static async pegaUmaPessoa(req, res) {
         const { id } = req.params
         try {
@@ -24,11 +26,37 @@ class PessoaController {
         }
     }
 
+    // criar um registro
     static async criaPessoa(req, res) {
         const novaPessoa = req.body
         try {
             const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
             return res.status(200).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // atualizar um registro
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } })
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // deletar um registro
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({ mensagem: `id ${id} deletado` })
+
         } catch (error) {
             return res.status(500).json(error.message)
         }
